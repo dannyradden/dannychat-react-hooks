af63f356b050db7ab4aa3339fa091960571c7600
DannyRadden <dradden@gmail.com>
2019-06-05 19:46:26 -0600

    Start css file

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Chat from './Chat'
 import Login from './Login'
+import './App.css';
 
 const App = () => {
     const [user, setUser] = useState(null);
